c3f93b0769ed92d15de8017c6d7711778a2bf876
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-10-18 17:12:28 +0200

    Ajout de toggle , création des boutons de réponse

--- COURS P2 JS-A-to-Z/cours/index.js
+++ COURS P2 JS-A-to-Z/cours/index.js
@@ -1,9 +1,29 @@
 // document.querySelector('h4').style.background = "yellow";
 
 const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
 
 console.log(questionContainer);
+console.log(btn1);
+console.log(btn2);
 
 questionContainer.addEventListener("click", () => {
   console.log("Click !");
+  questionContainer.classList.toggle("question-clicked");
 });
+
+btn1.addEventListener("click", () => {
+  response.style.visibility = "visible";
+  response.style.opacity = "1";
+  response.style.background = "green";
+});
+
+btn2.addEventListener("click", () => {
+  response.style.visibility = "visible";
+  response.style.opacity = "1";
+  response.style.background = "red";
+});
+
+//---------------------------------------------------------
