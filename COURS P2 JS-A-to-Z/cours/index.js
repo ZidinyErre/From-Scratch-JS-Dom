a9f58b26cb110f8dea159084e13574bdf42e124e
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-10-26 16:09:11 +0200

    Localisation

--- COURS P2 JS-A-to-Z/cours/index.js
+++ COURS P2 JS-A-to-Z/cours/index.js
@@ -233,3 +233,32 @@ document.body.addEventListener("click", (e) => {
 // window.onload = () => {
 //   location.href = "http://twitter.fr";
 // };
+
+// Navigator
+// console.log(navigator.userAgent);
+
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0
+// };
+
+// function success(pos) {
+//   var crd = pos.coords;
+
+//   console.log('Votre position actuelle est :');
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude : ${crd.longitude}`);
+//   console.log(`La précision est de ${crd.accuracy} mètres.`);
+// }
+
+// function error(err) {
+//   console.warn(`ERREUR (${err.code}): ${err.message}`);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error, options);
+
+// History 
+// console.log(history);
+// window.history.back();
+// history.go(-2); Celui la a pas trop marcher 
