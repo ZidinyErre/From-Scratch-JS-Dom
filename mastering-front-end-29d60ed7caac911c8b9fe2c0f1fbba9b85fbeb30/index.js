502f5a6cc6660ec634b50d90211c98af3de7d592
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-10-27 15:21:21 +0200

    TP1 rond aligné

--- mastering-front-end-29d60ed7caac911c8b9fe2c0f1fbba9b85fbeb30/index.js
+++ mastering-front-end-29d60ed7caac911c8b9fe2c0f1fbba9b85fbeb30/index.js
@@ -5,3 +5,21 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+const mousemove1 = document.querySelector(".mousemove1");
+const mousemove2 = document.querySelector(".mousemove2");
+const mousemove3 = document.querySelector(".mousemove3");
+
+// console.log(mousemove);
+
+window.addEventListener("mousemove", (e) => {
+    mousemove1.style.left = e.pageX + "px";
+    mousemove1.style.top = e.pageY + "px";
+
+    // mousemove2.style.left = e.pageX + "px";
+    // mousemove2.style.top = e.pageY + "px";
+
+    // mousemove3.style.left = e.pageX + "px";
+    // mousemove3.style.top = e.pageY + "px";
+
+});
