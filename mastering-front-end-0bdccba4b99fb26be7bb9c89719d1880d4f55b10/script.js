08a33ba9f268763a6239e566e2921bfbb33a2a6c
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-10-29 02:28:01 +0200

    TP3 corrigé

--- mastering-front-end-0bdccba4b99fb26be7bb9c89719d1880d4f55b10/script.js
+++ mastering-front-end-0bdccba4b99fb26be7bb9c89719d1880d4f55b10/script.js
@@ -11,23 +11,43 @@
 const navbar = document.querySelector("nav");
 const image = document.querySelector("#imgImprovise");
 const popup = document.querySelector(".pop-up");
+let playOnce = true ;
 
 console.log(popup);
 
 window.addEventListener("scroll", () => {
-
   if (window.scrollY > 200) {
     navbar.style.height = "55px";
   }
+  // T'aurais pu faire un else ici
   if (window.scrollY < 142) {
     navbar.style.height = "90px";
   }
-  if (window.scrollY > 350) {
+  // Pas tout a fait vrai car la height change selon les tailles d'écran !!
+  // if (window.scrollY > 350) {
+  //   image.style.opacity = 1;
+  //   image.style.transform = "none";
+  // }
+
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+  if (scrollValue > 0.45) {
     image.style.opacity = 1;
     image.style.transform = "none";
   }
-  if (window.scrollY >= 1632){
+  
+  // Même choses 
+  // if (window.scrollY >= 1632) {
+    if (scrollValue > 0.85 && playOnce) {
     popup.style.opacity = 1;
     popup.style.transform = "none";
+    playOnce = false;
   }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = 0;
+  popup.style.transform = "translateX(500px)";
+
 });
