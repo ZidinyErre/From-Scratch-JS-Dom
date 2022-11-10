32726501e0107cbb7ed9bce36c12dd7fba713bbe
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-11-10 19:48:33 +0100

    P3 TP 5 début

---
+++ P3 TP5/index.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+let x ;
+let y ;
+let z ;
+
+
+
+let inter = setInterval(() => {
+    Math.floor(Math.random() * 255);
+}, 1000);
+
+console.log(inter);
