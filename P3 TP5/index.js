a7e5d3c8187c93bd3aca37f9bdb3ed3f8f5d39cf
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-11-11 14:53:19 +0100

    P3 TP 5 corrigé

--- P3 TP5/index.js
+++ P3 TP5/index.js
@@ -1,12 +1,36 @@
-const container = document.querySelector(".container");
-let x ;
-let y ;
-let z ;
+// const container = document.querySelector(".container");
+// let random1 = Math.floor(Math.random() * 255);
+// let random2 = Math.floor(Math.random() * 255);
+// let random3 = Math.floor(Math.random() * 255);
+// let x = 0;
 
+// let inter = setInterval(() => {
+//   container.style.backgroundColor = `rgb(${random1},${random2},${random3})`;
+// //   if (++x === 1000) {
+// //     window.clearInterval(inter);
+// //   }
+// }, 1000, 100);
 
+// // let inter = window.onload = ()  => {
+// //     setInterval(() => {
+// //         Math.floor(Math.random() * 255)
+// //     }, 1000)
+// // };
 
-let inter = setInterval(() => {
-    Math.floor(Math.random() * 255);
-}, 1000);
+// console.log(random1);
+// console.log(random2);
+// console.log(random3);
+// console.log(x);
 
-console.log(inter);
+function getColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    
+    const color = `rgb(${r},${g}, ${b})`;
+    
+    document.body.style.background = color;
+    document.body.innerHTML = `<h2> ${color}</h2>`;
+}
+
+setInterval(getColor, 1000);
