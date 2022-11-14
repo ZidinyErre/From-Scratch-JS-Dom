9208fc11ce488c2ad52f876d7838fd2b6594ae87
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-11-14 18:53:29 +0100

    Début P3 TP6

---
+++ P3 TP6/index.js
@@ -0,0 +1,10 @@
+// Créer un form pour taper les todos dans le html
+
+//Ajouter sur le dom ce qui est tapé dans l'input à la validation du form
+// monInput.value
+
+// Supprimer un todo lorsque l'on click dessus (.remove())
+
+// Stocker dans le local storage la liste
+
+// Consulter le local storage au lancement de l'application pour rajouter les todos
