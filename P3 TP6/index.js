fc1fa6c424c758304d9784e9f75f306c2594a5a1
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-11-21 12:08:04 +0100

    Correction P3 TP6

--- P3 TP6/index.js
+++ P3 TP6/index.js
@@ -9,31 +9,58 @@
 
 // Consulter le local storage au lancement de l'application pour rajouter les todos
 
-const monInput = document.getElementById("todo");
-// const container = document.querySelector(".container");
-const form = document.querySelector(".myForm");
+// const monInput = document.getElementById("todo");
+// // const container = document.querySelector(".container");
+// const form = document.querySelector(".myForm");
 
-console.log(form);
+// console.log(form);
 
-monInput.addEventListener("keypress", (e) => {
-  console.log(e.key  + "2");
-  // let data = [];
-  if (e.key === "Enter") {
-    e.preventDefault();
-  // console.log(monInput.value);
-    document.body.innerHTML += `<span> ${monInput.value} </span>`
-  }
+// monInput.addEventListener("keypress", (e) => {
+//   console.log(e.key  + "2");
+//   // let data = [];
+//   if (e.key === "Enter") {
+//     e.preventDefault();
+//   // console.log(monInput.value);
+//     document.body.innerHTML += `<span> ${monInput.value} </span>`
+//   }
   
+// });
+
+const form = document.querySelector("form");
+
+// storage part
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+
+  } else {
+    list.innerHTML = `<li> Cliquez sur un todo pour le supprimer </li>`;
+  }
+}
+
+window.addEventListener("load", getTodos);
+// getTodos();
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+
+list.innerHTML += `<li>${item.value}</li>`;
+item.value = "";
+storeList();
+});
+
+list.addEventListener("click", (e) =>{
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+
+  };
+  storeList();
 });
 
 
-
-// if (e.code === "Enter") {
-//   e.preventDefault();
-// let formData = new FormData(this);
-// let input = formData.get(monInput);
-
-// console.log(input);
-
-//   // container.innerHTML += `<span> ${e.code} </span>`
-// }
