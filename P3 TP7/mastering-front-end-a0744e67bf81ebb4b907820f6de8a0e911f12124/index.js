86c15f445c297750356030650879ac1e31cf423a
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-11-21 23:56:37 +0100

    début  P3 TP7

---
+++ P3 TP7/mastering-front-end-a0744e67bf81ebb4b907820f6de8a0e911f12124/index.js
@@ -0,0 +1,19 @@
+// Créer la logique de compte à rebours
+
+// choice.value = 5 minutes
+
+// choice.value * 60 = totalSeconds
+
+// let minutes = Math.floor(totalSeconds / 60);
+// let seconds = totalSeconds % 60
+
+// Créer un événement à la validation du form pour lancer le compte à rebours
+
+const choix = document.querySelector("#choice");
+const go = document.querySelector("#start");
+
+go.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log(choice.value);
+});
+
