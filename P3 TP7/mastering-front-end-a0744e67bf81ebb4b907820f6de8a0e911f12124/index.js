d5934e4263f90b4fa48f37d8754d66fddeb87688
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-11-27 18:20:39 +0100

    Correction TP7

--- P3 TP7/mastering-front-end-a0744e67bf81ebb4b907820f6de8a0e911f12124/index.js
+++ P3 TP7/mastering-front-end-a0744e67bf81ebb4b907820f6de8a0e911f12124/index.js
@@ -1,5 +1,6 @@
 // Créer la logique de compte à rebours
 
+
 // choice.value = 5 minutes
 
 // choice.value * 60 = totalSeconds
@@ -8,29 +9,77 @@
 // let seconds = totalSeconds % 60
 
 // Créer un événement à la validation du form pour lancer le compte à rebours
+// let choix = document.getElementById("choice").value;
 
-const choix = document.querySelector("#choice");
-const go = document.querySelector("#start");
-const div = document.querySelector("#countdownDisplay");
-// function set() {
-//     setInterval()
-// }
-// let temps = choice.value;
-let minutes;
-let seconds;
+// const go = document.querySelector("#start");
+// const div = document.querySelector("#countdownDisplay");
 
-function diminuerTemps(params) {
-    setInterval(()=>{
-        // console.log();
-        div.innerHTML = `<div> ${ choice.value--} </div>`;
+// let totalSeconds = choix * 60;
+// let minutes = Math.floor(totalSeconds / 60);
+// let seconds = Math.floor(totalSeconds % 60 );
+// function diminuerTemps() {
+//     setInterval(()=>{
+//         // console.log();
+//         div.innerHTML = `<div> ${ minutes--} : ${ seconds --} </div>`;
 
-    }, 1000)
-};
+//     }, 1000)
+// };
+// // function input() {
+// //     let choix = document.getElementById("choice").value;
+// //     alert(choix)
+// // };
 
-go.addEventListener("click", (e)=>{
+// go.addEventListener("click", (e)=>{
+//     e.preventDefault();
+//     // input();
+
+//     console.log(choix);
+//     console.log(choice.value);
+
+//     div.innerHTML = `<div>  ${ minutes--} : ${ seconds --}  </div>`;
+
+//     diminuerTemps();
+// });
+
+
+//  correction
+
+let totalSeconds ;
+let interval ; 
+
+const song = () => {
+  const audio = new Audio();
+  audio.src = "./song/legendary.mp3";
+  audio.play();
+}
+
+function countDown() {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    const sec = seconds < 10 ? "0" + seconds : seconds;
+
+    countdownDisplay.textContent =`${minutes}   :  ${sec}` ;
+
+if (totalSeconds > 0) {
+    totalSeconds--;
+} else {
+    countdownDisplay.textContent = "C'est terminé !";
+    clearInterval(interval);
+
+    song();
+}
+    
+}
+
+form.addEventListener("submit", (e) =>{
     e.preventDefault();
-    div.innerHTML = `<div> ${ choice.value--} </div>`;
-
-    diminuerTemps();
+    if(isNaN(choice.value)){
+        alert("entrez un chiffre avant que je me fâche")
+    } else {
+        totalSeconds = choice.value * 60;
+        choice.value = "";
+        clearInterval(interval);
+        interval = setInterval(countDown, 100);
+    }
 });
-
