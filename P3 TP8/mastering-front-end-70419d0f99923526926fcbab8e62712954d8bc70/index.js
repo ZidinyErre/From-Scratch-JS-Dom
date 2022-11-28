e934b6888536d811a12133b3929cdb47340a2e29
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-11-28 13:11:19 +0100

    Début TP8

---
+++ P3 TP8/mastering-front-end-70419d0f99923526926fcbab8e62712954d8bc70/index.js
@@ -0,0 +1,22 @@
+// Convert today date to input format
+const today  = new Date().toISOString().split("T")[0];
+start_date.value = today;
+start_date.min = today;
+
+// Tomorrow date calc
+let tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+// Convert to input format 
+let tomorrowFormat = tomorrow.toISOString().split("T")[0];
+end_date.value = tomorrowFormat;
+end_date.min = tomorrowFormat;
+
+start_date.addEventListener("change", (e) => {
+    let day = new Date(e.target.value);
+
+if(end_date.value < start_date.value) {
+    day.setDate(day.getDate() + 1);
+    console.log(day);
+}
+});
